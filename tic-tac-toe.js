d3314095b497306f9409b9da84daae81520aa286
gisimclean@gmail.com <138517546+GiselleDM@users.noreply.github.com>
2023-10-21 16:24:43 -0500

    exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Get all the squares
   const squares = document.querySelectorAll("#board div");
+  const status = document.getElementById("status");
+  let currentPlayer = "X"; // Initialize the current player as "X"
 
-  // Add the "square" class to each square
   squares.forEach((square) => {
     square.classList.add("square");
+
+    square.addEventListener("click", () => {
+      if (!square.classList.contains("X") && !square.classList.contains("O")) {
+        // Check if the square is empty
+        square.classList.add(currentPlayer); // Add the "X" or "O" class for styling
+        square.textContent = currentPlayer; // Add the X or O text
+
+        // Check for a win or draw here (implement this logic later)
+
+        // Switch the current player for the next turn
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+        // Update the status message
+        status.textContent = `Player ${currentPlayer}'s turn`;
+      }
+    });
   });
 });
